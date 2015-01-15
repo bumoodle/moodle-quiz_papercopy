5b21f3284a2e7f340a118b9be874d0f710bcfcad
Ray Morris <Ray.Morris@teex.tamu.edu>
2015-01-15 13:56:55 -0600

    temporarily skip check for non-zero number of questions

--- report.php
+++ report.php
@@ -108,11 +108,15 @@ class quiz_papercopy_report extends quiz_default_report
         //--- display the report:
 
         //if there are no questions in the quiz, display an error message
+        // TODO Ray - see if $quizobj->get_questions() is a suitable replacement
+        /*
         if (!quiz_questions_in_quiz($quiz->questions)) 
             echo quiz_no_questions_message($quiz, $cm, $this->context);
+        */
 
+        if (false) {
         //otherwise, if we have no action, display the index page
-        else if (!$action) 
+        } else if (!$action)
         {
             $this->maintain_batches();
             $this->display_index();
